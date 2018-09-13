f9bc867fb33f87e6e18581a0019f4aa719b0c180
RichFisher <rich.fishernc@gmail.com>
2018-09-13 11:23:16 -0500

    Im a goddam genius

--- main.js
+++ main.js
@@ -92,8 +92,23 @@ const year = {
     December: 31
 }
 
+const monthObject = {
+    'January': year.January,
+    'February': year.February,
+    'March': year.March,
+    'April': year.April,
+    'May': year.May,
+    'June': year.June,
+    'July': year.July,
+    'August': year.August,
+    'September': year.September,
+    'October': year.October,
+    'November': year.November,
+    'December': year.December
+}
+
 let date = 0;
-let dateCard = `<h4 class='dateCardHeader'>${date}</h4>`;
+let dateCard = '';
 
 const printToDom = (stringToPrint, divId) => {
     document.getElementById(divId).innerHTML = stringToPrint;
@@ -125,23 +140,50 @@ function eventLister(month) {
 
 function monthCardSelector() {
     document.getElementById('calendarMenu').addEventListener('click', function() {
-        let menuSelect = event.target.id;
-        let monthSelect = year.April;
+        let menuSelect = document.getElementById('select').value;
+        let monthSelect = monthObject[menuSelect];
+        date = 0;
+        for(let i = 0; i < 31; i++) {
+            date++;
+            dateCard = `<h4 class='dateCardHeader'></h4>`;
+            let dateString = ''+date;
+            printToDom(dateCard, 'dateDiv'+dateString);            
+        }
         date = 0;
         for(let i = 0; i < monthSelect; i++) {
+            date++;
             dateCard = `<h4 class='dateCardHeader'>${date}</h4>`;
             let dateString = ''+date;
             printToDom(dateCard, 'dateDiv'+dateString);            
-            date++;
         }
     })
 }
 
+function eventListerCalendar(month) {
+    let eventClass = '';
+    for(let i = 0; i < events[month].length; i++) {
+        let dateString = events[month][i].date;
+        let eventString = document.getElementById('dateDiv'+dateString).innerHTML;
+        eventString +=  `<div class=${eventClass}>`
+        eventString +=      `<h3 class='jsEventsHeader'>${events[month][i].name}</h3>`
+        eventString +=      `<p class='jsEventsPar'>Date: ${month} ${events[month][i].date}</p>`
+        eventString +=      `<p class='jsEventsPar'>Location: ${events[month][i].venue}</p>`
+        eventString +=      `<p class='jsEventsPar'>ShowTime: ${events[month][i].time}</p>`
+        eventString +=      `<p class='jsEventsPar'>${events[month][i].description}</p>`
+        eventString +=  `</div>`;
+        printToDom(eventString, 'dateDiv'+dateString);
+    };    
+}
+
 function dateCardSelector() {
-    
+    document.getElementById('calendarMenu').addEventListener('click', function() {
+        let menuSelect = document.getElementById('select').value;
+        eventListerCalendar(menuSelect);
+    })
 }
 
 eventLister('April');
 eventLister('May');
 eventLister('June');
 monthCardSelector();
+dateCardSelector();

a4e1f3fa1f725b7363c546779e0247f0628e48b4
Ryan Nelson <ryannelson881995@gmail.com>
2018-09-15 09:58:08 -0500

    looking at pull request

--- main.js
+++ main.js
@@ -4,45 +4,73 @@ let dethalbumsong = ["Murmaider", "Go into the Water", "Awaken", "Bloodrocuted",
 let dethalbum2song = ["Bloodlines", "The Gears", "Burn the Earth", "Laser Cannon Deth Sentence", "Black Fire Upon Us", "Dethsupport", "The Cyborg Slayers", "I Tamper with the Evidence at the Murder Site of Odin", "Murmaider II: The Water God", "Comet Song", "Symmetry", "Volcano"]
 let dethalbum3song = ["I Ejaculate Fire", "Crush the Industry", "Andromeda", "The Galaxy", "Starved", "Killstardo Abominate", "Ghostqueen", "Impeach God", "Biological Warfare", "Skyhunter", "The Hammer", "Rejoin" ]
 
-let Album1 = {
-    name:"Evolution",
-    image:"./images/Evolution.jpg",
-    release:"10/19/2018",
-    songs:`${evolutionsong}`
-}
-let Album2 = {
-    name:"Dethalbum III",
-    image:"./images/DethalbumIII.jpg",
-    release:"10/16/2012",
-    songs:`${dethalbum3song}`
-}
-let Album3 = {
-    name:"Asylum",
-    image:"./images/asylum.jpg",
-    release:"8/31/2010",
-    songs:a`${asylumsong}`
-}
-let Album4 = {
-    name:"Dethalbum II",
-    image:"./images/dethalbumII.jpg",
-    release:"9/29/2009",
-    songs:`${dethalbum2song}`
-}
-let Album5 = {
-    name:"Dethalbum",
-    image:"./images/dethalbum.jpg",
-    release:"9/25/2007",
-    songs:`${dethalbumsong}`
+let albumspage = {
+album: [
+    {
+            name:"Evolution",
+            image:"./images/Evolution.jpg",
+            release:"10/19/2018",
+            songs:`${evolutionsong}`
+    },
+    {
+        name:"Dethalbum III",
+        image:"./images/DethalbumIII.jpg",
+        release:"10/16/2012",
+        songs:`${dethalbum3song}`
+    },
+    {
+        name:"Asylum",
+        image:"./images/asylum.jpg",
+        release:"8/31/2010",
+        songs:`${asylumsong}`
+    },
+    {
+        name:"Dethalbum II",
+        image:"./images/dethalbumII.jpg",
+        release:"9/29/2009",
+        songs:`${dethalbum2song}`
+    },
+    {
+        name:"Dethalbum",
+        image:"./images/dethalbum.jpg",
+        release:"9/25/2007",
+        songs:dethalbumsong
+    }
+]
 }
 
-
-console.log(Album5)
 
 
 const printToDom = (stringToPrint, divId) => {
     document.getElementById(divId).innerHTML = stringToPrint;
 }
 
-const albumspage = () => {
-    
+const albumspageprint = () => {
+    const newString = `<p>${albumspage.album}</p>`
+    printToDom(newString,'albumscard')
 }
+
+const albumspageprintbilder = () => {
+    let newString = '';
+    for(let i = 0; i < albumspage.album.length; i++) {
+        newString +=``
+    }
+}
+
+const statementsStringBuilder = () => {
+    let newString = '';                               
+    for(let i = 0; i < elizabethSanger.statements.length; i++) {
+        newString +=`<div class="statement">`;
+        newString +=    `<h4>${elizabethSanger.statements[i].category}: ${elizabethSanger.statements[i].statement}</h4>`;
+        newString +=`</div>`;
+    }
+    printToDom(newString, 'statements');
+};
+
+
+
+
+
+
+
+albumspageprint()

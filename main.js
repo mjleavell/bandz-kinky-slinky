f9c394c6923dfa8ae2c1096fee823e58cd5055c4
Raymond Arceneaux <raymond.techcoder@gmail.com>
2018-09-17 08:17:14 -0500

    add click function and div bkgrd

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-// PRINT TO DOM FUNCTION
-const printToDom = (stringToPrint, divId) => {
-    document.getElementById(divId).innerHTML = stringToPrint;
-}
+
 
 // NAVBAR FUNCTION
 function buttonsHover() {
@@ -19,13 +16,13 @@ function buttonsHover() {
 }
 
 
-let mem1 = {name:'Ben Rector',instrument:'Guitar',favoriteSong:'Born in the USA',picture:'https://www.billboard.com/files/media/Ben-Rector-bb21-hot-100-pr-2016-billboard-1548.jpg'};
+let mem1 = {id:'mem1',name:'Ben Rector',instrument:'Guitar',favoriteSong:'Born in the USA',picture:'https://www.billboard.com/files/media/Ben-Rector-bb21-hot-100-pr-2016-billboard-1548.jpg'};
 
-let mem2 = {name:'Terry',instrument:'Bass',favoriteSong:'In the Air Tonight', picture:'https://pmcvariety.files.wordpress.com/2018/08/terry-crews1.jpg'};
+let mem2 = {id:'mem2',name:'Terry',instrument:'Bass',favoriteSong:'In the Air Tonight', picture:'https://pmcvariety.files.wordpress.com/2018/08/terry-crews1.jpg'};
 
-let mem3 = {name:'Lady Gaga',instrument:'Lead Singer',favoriteSong:'Great Balls of Fire',picture:'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hbz-the-list-best-beauty-looks-of-the-year-lady-gaga-gettyimages-634472516-1512514124.jpg'};
+let mem3 = {id:'mem3',name:'Lady Gaga',instrument:'Lead Singer',favoriteSong:'Great Balls of Fire',picture:'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hbz-the-list-best-beauty-looks-of-the-year-lady-gaga-gettyimages-634472516-1512514124.jpg'};
 
-let mem4 = {name:'Questlove',instrument:'Drums',favoriteSong:'Hound Dog' ,picture:'https://www.phillymag.com/wp-content/uploads/sites/3/2012/08/questloveuncropped1.jpg'};
+let mem4 = {id:'mem4',name:'Questlove',instrument:'Drums',favoriteSong:'Hound Dog' ,picture:'https://media.pitchfork.com/photos/59c2b0b371249b1b0091dc7e/1:1/w_300/Questlove_Photo%20by%20Jason%20Kempin:WireImage_463076764%202.jpg'};
 
 const members = [mem1,mem2,mem3,mem4];
 
@@ -35,12 +32,37 @@ const printToDom =(stringToPrint,divId) =>{
     selectedDiv.innerHTML += stringToPrint;
 };
 
-// function to loop thru info
+
+const getMemberById = (id) =>{
+    for (let i =0;i<members.length;i++){
+        let member = members[i];
+        if (member.id === id){
+            return member
+        }
+    }
+
+
+}
+const needHelp = (event) => {
+    if (event.target.tagName !== 'IMG'){
+        return
+    }
+    let member = getMemberById(event.target.parentElement.id) 
+    let bandInfo = "";
+    bandInfo += `<p>${member['name']}</p>`
+    bandInfo += `<p>${member['instrument']}</p>`;
+   printToDom(bandInfo,'test');
+}
+
+
+// function to loop thru band member info
+
+document.getElementById('picture-container').addEventListener('click',needHelp)
 
 const nfl = () =>{
     for (let i=0;i<members.length;i++){
-        let imageString = `<div id="mem${i+1}">`;
-        imageString += `<img class="mem-pic" src="${members[i].picture}" alt="${members[i].name}" onclick="">`
+        let imageString = `<div id="${members[i].id}">`;
+        imageString += `<img class="mem-pic" src="${members[i].picture}" alt="${members[i].name}">`
         imageString += `</div>`
         printToDom(imageString,'picture-container');
     }; 
@@ -49,10 +71,14 @@ nfl();
 
 
 
-// onclick event function
 
-// var mem1Info = document.getElementById('pic1');
-// mem1Info.addEventListener('click',)
+
+
+
+
+
+
+
 
 // CALL FUNCTIONS 
-buttonsHover();
+// buttonsHover();
